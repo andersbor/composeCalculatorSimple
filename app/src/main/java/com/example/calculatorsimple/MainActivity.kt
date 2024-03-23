487d85a8703c6ae0fd4d9fae0e073ed674c786b4
andersbor <anbo@zealand.dk>
2024-03-23 15:52:09 +0100

    higher order functions for calculations

--- app/src/main/java/com/example/calculatorsimple/MainActivity.kt
+++ app/src/main/java/com/example/calculatorsimple/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -26,12 +27,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorSimpleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting("Android")
                     Calculator()
                 }
             }
@@ -46,6 +45,11 @@ fun Calculator() {
     var error1 by remember { mutableStateOf(false) }
     var error2 by remember { mutableStateOf(false) }
     var result by remember { mutableStateOf("") }
+    //var op: (Double, Double) -> Double = { _, _ -> 0.0 }
+    //var operation by remember { mutableStateOf(op) }
+    var buttonsEnabled = !error1 && !error2
+    val plus: (Double, Double) -> Double = { n1, n2 -> n1 + n2 }
+    val minus: (Double, Double) -> Double = { n1, n2 -> n1 - n2 }
     val onTextChange1 = { text: String ->
         numberStr1 = text
         error1 = text.toDoubleOrNull() == null
@@ -54,13 +58,14 @@ fun Calculator() {
         numberStr2 = text
         error2 = text.toDoubleOrNull() == null
     }
-    val calculate = {
+    val calculate = { operation: (Double, Double) -> Double ->
         if (error1 || error1) {
             result = "Invalid input"
         } else {
             val number1 = numberStr1.toDouble()
             val number2 = numberStr2.toDouble()
-            result = "Result: " + (number1 + number2).toString()
+            val res: Double = operation(number1, number2)
+            result = "Result: $res"
         }
     }
 
@@ -77,8 +82,31 @@ fun Calculator() {
             label = "Enter a number",
             error = error2
         )
-        Button(onClick = calculate) {
-            Text("Add")
+        Row {
+            Button(
+                onClick = { calculate(plus) },
+                enabled = buttonsEnabled
+            ) {
+                Text("+")
+            }
+            Button(
+                onClick = { calculate(minus) },
+                enabled = buttonsEnabled
+            ) {
+                Text("-")
+            }
+            Button(
+                onClick = { calculate { n1, n2 -> n1 * n2 } },
+                enabled = buttonsEnabled
+            ) {
+                Text("*")
+            }
+            Button(
+                onClick = { calculate { n1, n2 -> n1 / n2 } },
+                enabled = buttonsEnabled
+            ) {
+                Text("/")
+            }
         }
         Text(result)
     }
@@ -99,7 +127,6 @@ fun NumberTextField(
         //isError = value.toDoubleOrNull() == null
         isError = error
     )
-
 }
 
 @Preview(showBackground = true)
